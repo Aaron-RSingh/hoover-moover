11f2973cef3e0945590c8b7ad70b11ce122bb32e
Aaron <aaron.ramharacksingh@gmail.com>
2019-12-12 16:43:20 +0000

    added roomba movement function

--- roomba.js
+++ roomba.js
@@ -1,70 +1,6 @@
-let sortData = function(string) {
-  console.log(string);
-
-  // split data into an array, seperating elements by spaces
-  // !NOTE! There is a space at the end of each line in the input.txt file
-
-  const array = string.split(" ");
-  console.log(array);
-
-  // setting directions to a variable and removing them form input data
-
-  let directions = array[array.length - 1];
-  console.log(directions);
-  array.pop();
-
-  // setting room dimensions to variables and removing them from input data
-
-  let roomWidth = array[0];
-  array.shift();
-  let roomHeight = array[0];
-  array.shift();
-
-  console.log("room width" + " = " + roomWidth);
-  console.log("room height" + " = " + roomHeight);
-
-  // setting roomba start positions to variables and removing them from input data
-
-  let roombaXStart = array[0];
-  array.shift();
-  let roombaYStart = array[0];
-  array.shift();
-
-  console.log("roomba X Start" + " " + roombaXStart);
-  console.log("roomba Y Start" + " " + roombaYStart);
-
-  // remaining input data consist of pairs of coordinates for dirt piles
-  // send remaining input data into coordinatesArray function
-
-  let dirtPatches = this.coordinatesArray(array);
-
-  let numberOfDirtPatches = dirtPatches.length;
-  console.log(numberOfDirtPatches + " " + "dirt patches");
-  colourDirt(dirtPatches);
-};
-
-// function to sort data into pairs and into each pairs' coordinate set
-
-function coordinatesArray(array) {
-  let results = [];
-  while (array.length) {
-    results.push(array.splice(0, 2));
-  }
-  return results;
-}
-
-// function to colour dirt tiles
-
-function colourDirt(array) {
-  console.log(array);
-  index = 0;
-  while (index < array.length) {
-    console.log("this will be where I change the dirt tiles to brown");
-    index++;
-  }
-}
-
 // function to fetch input data from input.txt and send data to be "sorted"
+
+fetchingInput();
 
 function fetchingInput() {
   fetch("input.txt")
@@ -72,4 +8,77 @@ function fetchingInput() {
     .then(text => sortData(text));
 }
 
-fetchingInput();
+// function to sort data into room room size, roomba start position, dirt positions and directions
+
+let sortData = function(string) {
+  // split data into an array, seperating elements by new lines
+
+  const data = string.split("\n");
+
+  // setting directions to a variable and removing them from input data
+
+  let directions = data.pop();
+
+  // converting rest of data to numbers instead of strings
+
+  let sortedData = data.map(pair => {
+    const [x, y] = pair.split(" ").map(Number);
+    return { x, y };
+  });
+
+  // setting sortedData to an object with keys as the room dimensions, roomba start position and an array of dirt piles
+  let [roomSize, roombaStartPosition, ...dirtPiles] = sortedData;
+  console.log({ roomSize, roombaStartPosition, dirtPiles, directions });
+
+  // setting numeric value to roomba start position
+
+  let numericalRoombaStartPosition =
+    roombaStartPosition.x + 5 * roombaStartPosition.y;
+  console.log({ numericalRoombaStartPosition });
+  convertDirtToNumbers(dirtPiles);
+
+  // function to call roomba movemtent
+  directionalMovement(directions, numericalRoombaStartPosition, roomSize);
+};
+
+// function to identify numeric value for dirt tiles
+
+const dirtArray = [];
+function convertDirtToNumbers(data) {
+  data.map(dirtPile => {
+    const dirtValue = dirtPile.x + 1 + 5 * dirtPile.y;
+    dirtArray.push(dirtValue);
+  });
+  console.log({ dirtArray });
+}
+
+// function to describe direcitonal movement
+
+function directionalMovement(directions, startpoint, roomSize) {
+  let cleanupCount = 0;
+  let directionArray = [...directions];
+  let roombaCurrentPosition = startpoint;
+  directionArray.forEach(direction => {
+    if (
+      direction == "N" &&
+      roombaCurrentPosition <= roomSize.x * (roomSize.y - 1)
+    ) {
+      roombaCurrentPosition += 5;
+    } else if (direction == "S" && roombaCurrentPosition > roomSize.x) {
+      roombaCurrentPosition -= 5;
+    } else if (direction == "E" && roombaCurrentPosition % roomSize.x !== 0) {
+      roombaCurrentPosition += 1;
+    } else if (
+      direction == "W" &&
+      (roombaCurrentPosition - 1) % roomSize.x !== 0
+    ) {
+      roombaCurrentPosition -= 1;
+    }
+    if (dirtArray.includes(roombaCurrentPosition)) {
+      cleanupCount += 1;
+    }
+    console.log({ roombaCurrentPosition, roomSize, direction });
+  });
+  let roombaFinalPosition = roombaCurrentPosition;
+  console.log({ roombaFinalPosition, cleanupCount });
+}
